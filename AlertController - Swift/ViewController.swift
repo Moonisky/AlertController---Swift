ad5507853ba9a829034aaa50080f5db2ea3df019
SemperIdem <semper_idem@sumomomatoMacBook-Air.local>
2014-11-19 00:17:14 +0800

    Initial Commit

---
+++ AlertController - Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlertController - Swift
+//
+//  Created by Semper Idem on 14-11-19.
+//  Copyright (c) 2014年 星夜暮晨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
